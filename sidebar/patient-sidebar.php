ca90cd215d6ae09c77330e2037a0c0a6b54282bc
harsh hemani <hhemani365@gmail.com>
2026-02-01 02:55:19 +0530

    sidebar for patient dashboard

---
+++ sidebar/patient-sidebar.php
@@ -0,0 +1,44 @@
+<nav class="navbar fixed-top navbar-expand-lg" id="nav-home">
+    <div class="container-fluid">
+        <button class="px-5" type="button" data-bs-toggle="offcanvas" id="menu" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+            <i class="bi bi-list fs-2 fw-bolder"></i>
+        </button>
+        <img src="./img/mediq.png" style="height: 60px;" class="mx-5" alt="">
+    </div>
+</nav>
+
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas-header" style="background-color: #fef9f9;">
+        <img src="./img/mediq.png" style="height: 70px;" class="mx-5" alt="">
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body" style="background-color: #fef9f9;">
+        <div class="navbar-nav mb-2 px-5 text-lg-center">
+            <a href="./doctor.php" type="none" class="nav-link">Dashboard</a>
+        </div>
+
+        <div class="navbar-nav mb-2 px-5 text-lg-center">
+            <a href="./doctor.php" type="none" class="nav-link">Book Appointments</a>
+        </div>
+
+        <div class="navbar-nav mb-2 px-5 text-lg-center">
+            <a href="./doctor.php" type="none" class="nav-link">Live Queue</a>
+        </div>
+
+        <div class="navbar-nav mb-2 px-5 text-lg-center">
+            <a href="./doctor.php" type="none" class="nav-link">My Appointment</a>
+        </div>
+
+        <div class="navbar-nav mb-2 px-5 text-lg-center">
+            <a href="./doctor.php" type="none" class="nav-link">Visit History</a>
+        </div>
+
+        <div class="navbar-nav mb-2 px-5 text-lg-center">
+            <a href="./doctor.php" type="none" class="nav-link">Profile Settings</a>
+        </div>
+
+        <div class="navbar-nav mb-2 px-5 text-lg-center">
+            <a href="./doctor.php" type="none" class="nav-link">Logout</a>
+        </div>
+    </div>
+</div>
