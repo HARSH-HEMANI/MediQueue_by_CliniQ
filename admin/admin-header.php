b783840a8d24baea8592f6aa63625e662c3ba09c
harsh hemani <hhemani365@gmail.com>
2026-02-07 23:52:27 +0530

    admin module header

---
+++ admin/admin-header.php
@@ -0,0 +1,18 @@
+<nav class="navbar fixed-top navbar-expand-lg" id="nav-home">
+    <div class="container-fluid px-4">
+
+        <button class="px-4" type="button" id="menu" data-bs-toggle="offcanvas"
+            data-bs-target="#adminSidebar">
+            <i class="bi bi-list fs-2"></i>
+        </button>
+
+        <a class="navbar-brand" id="logo" href="dashboard.php">
+            <img src="../img/mediq.png" style="height:55px;">
+        </a>
+
+        <div class="ms-auto fw-semibold">
+            Admin Panel
+        </div>
+
+    </div>
+</nav>
