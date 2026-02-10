3af22ff7de4dafde58b7acc8ff788b9435bd717f
harsh hemani <hhemani365@gmail.com>
2026-02-10 22:50:12 +0530

    updated page as per layout

--- admin/admin.php
+++ admin/admin.php
@@ -1,22 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MediQueue | Admin Dashboard</title>
-    <link rel="stylesheet" href="../css/bootstrap/css/bootstrap.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    <script src="../css/bootstrap/js/bootstrap.bundle.js"></script>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/admin.css">
-</head>
-
-<body>
-
-    <?php include "admin-header.php"; ?>
-    <?php include "admin-sidebar.php"; ?>
-
+<?php
+$content_page = 'admin-home';
+ob_start();
+?>
     <main class="admin-dashboard" style="margin-top:20px;">
         <div class="container">
 
@@ -124,9 +109,7 @@
 
         </div>
     </main>
-
-
-
-</body>
-
-</html>
+<?php
+$content = ob_get_clean();
+include './admin-layout.php';
+?>
