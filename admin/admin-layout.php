f9d5ecbaee615d26098115bf719c8c0b10625d75
harsh hemani <hhemani365@gmail.com>
2026-03-22 22:28:49 +0530

    admin layout

--- admin/admin-layout.php
+++ admin/admin-layout.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MediQueue | Admin Dashboard</title>
-    <link rel="stylesheet" href="../css/bootstrap/css/bootstrap.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bootstrap/css/bootstrap.css?v=vibrant">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css?v=vibrant" rel="stylesheet">
     <script src="../css/bootstrap/js/bootstrap.bundle.js"></script>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="../css/style.css?v=vibrant">
+    <link rel="stylesheet" href="../css/admin.css?v=vibrant_admin3">
 </head>
 
-<body>
+<body class="layout-with-sidebar">
 
     <?php include "admin-header.php"; ?>
     <?php include "admin-sidebar.php"; ?>
