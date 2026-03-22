1004a4de898f8862fcab717e599cf17b3d12eee6
harsh hemani <hhemani365@gmail.com>
2026-03-22 22:28:36 +0530

    mail test

---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+include_once './mailer/mailer.php';
+
+sendEmail("hhemani339@rku.ac.in", "Test Email from MediQueue", "<h1>Hello from MediQueue!</h1><p>This is a test email sent using PHPMailer.</p>");
