41b7da14fe354118594b63faee9b985a34937d16
harsh hemani <hhemani365@gmail.com>
2026-03-22 22:38:19 +0530

    patient layout

--- patient/patient-layout.php
+++ patient/patient-layout.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $content_page; ?></title>
-    <link rel="stylesheet" href="../css/patient.css">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/footer.css">
-    <link rel="stylesheet" href="../css/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../css/patient.css?v=vibrant">
+    <link rel="stylesheet" href="../css/style.css?v=vibrant">
+    <link rel="stylesheet" href="../css/footer.css?v=vibrant">
+    <link rel="stylesheet" href="../css/bootstrap/css/bootstrap.css?v=vibrant">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css?v=vibrant" rel="stylesheet">
     <script src="../css/bootstrap/js/bootstrap.bundle.js"></script>
 </head>
 
-<body>
+<body class="layout-with-sidebar">
     <?php include '../sidebar/patient-sidebar.php'; ?>
 
     <?php
@@ -22,6 +22,8 @@
         echo $content;
     }
     ?>
+
+
     <?php include '../footer.php'; ?>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="../js/validation.js"></script>
