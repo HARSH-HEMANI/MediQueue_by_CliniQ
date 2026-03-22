96f9d9f116b8fd7c5db0d6deb56a15c65e8c3ac6
harsh hemani <hhemani365@gmail.com>
2026-03-22 23:23:25 +0530

    --

--- doctor/logout.php
+++ doctor/logout.php
@@ -1,9 +1,7 @@
 <?php
-
 session_start();
-
-session_destroy();
+session_unset();      // FIX: clear all session variables first
+session_destroy();    // then destroy the session
 
 header("Location: ./login.php");
 exit();
-?>
